581d3e2d7cc5c670fcff9c24fa218ff07b7b4efa
Atharva Kadam <100138079+Atharva0402@users.noreply.github.com>
2024-03-09 16:03:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NotionKanban } from './components/NotionKanban'
+function App() {
+  return (
+    <div>
+      {/* <NotionKanban/> */}
+      <NotionKanban/>
+    </div>
+  )
+}
+
+export default App
