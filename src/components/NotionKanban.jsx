0588d1099afe85f15424178db82d3454a175c5e3
Atharva Kadam <100138079+Atharva0402@users.noreply.github.com>
2025-01-20 12:39:33 +0530

    Update NotionKanban.jsx
    change BeforeId to BeforeID

--- src/components/NotionKanban.jsx
+++ src/components/NotionKanban.jsx
@@ -218,10 +218,10 @@ const Card = ({ title, id, column, handleDragStart }) => {
 }
 
 
-const DropIndicator=({beforeId ,column})=>{
+const DropIndicator=({beforeID ,column})=>{
     return (
         <div 
-        data-before={beforeId || "-1"}
+        data-before={beforeID || "-1"}
         data-column={column}
         className="my-0.5 h-0.5 w-full bg-violet-400 opacity-0"/>
     )
